d13d74299053201330abc10a8823571012c83d65
Edison Villarta <edtvillarta@outlook.com>
2018-02-22 01:18:57 +0800

    change header views

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<title><?php echo $title_page; ?> | Market View Research</title>
+	<?php echo link_tag('assets/css/bootstrap.min.css'); ?>
+	<?php echo link_tag('assets/css/custom.css'); ?>
+	<!-- <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script> -->
+	<script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
+	
 </head>
 
