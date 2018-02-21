cb403189b58439f6b8c61fce2a52960c30327901
Edison Villarta <edtvillarta@outlook.com>
2018-02-22 01:18:24 +0800

    add custom javascript

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+/* login form validation */
+function validateEntries() 
+{
+	
+	var reg = /[a-zA-Z]{1}[a-zA-Z0-9]{4,19}$/
+	var str = document.getElementById("inputUser").value;
+	if (!(str.match(reg)))
+	{
+		document.getElementById("message").innerHTML = "Not a valid username. Please enter alpha numeric minimum of 5 characters and maximum of 20 characters and should start with alphabet.";
+		return false;	
+	}
+	
+	var reg = /^[A-Za-z]\w{5,}$/;
+	var str = document.getElementById("inputPass").value;
+	if (!(str.match(reg)))
+	{
+		document.getElementById("message").innerHTML = "Please enter a valid password!";
+		return false;	
+	}	
+	return true;
+}
