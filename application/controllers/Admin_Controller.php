dc210589a38b385c6743636931a8f95321450ed1
Edison Villarta <edtvillarta@outlook.com>
2018-02-21 23:04:13 +0800

    Add Login Functionality

--- application/controllers/Admin_Controller.php
+++ application/controllers/Admin_Controller.php
@@ -1,9 +1,66 @@
 <?php
 	class Admin_Controller extends CI_Controller {
-		function index() {
-			echo 'this is login index';
+		function __construct() {
+			parent::__construct();
+			$this->load->library('form_validation');
+			$this->load->model('admin');
 		}
-		public function login() {
+		public function loginAuth() {
+			$data = array();
+			//getting value from the login form
+			$data['username'] = $this->input->post('username');
+			$data['password'] = $this->input->post('password');
+			$this->form_validation->set_rules('username', 'Username', 'trim|required');
+			$this->form_validation->set_rules('password', 'Password', 'trim|required');
+			
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('error_msg', validation_errors());
+				redirect('/');
+			} else {
+				$result = $this->admin->login($data);
 
+				if($result){
+					$username = $this->input->post('username');
+					$result = $this->admin->readAdminInfo($username);
+					if($result){
+						$session_data = array(
+							'username' => $result[0]->name
+						);
+						$this->session->set_userdata('logged_in', $session_data);
+						
+						$this->session->set_flashdata('display_msg', 'Welcome');
+						redirect('/');
+							
+					}
+				}else{
+					$data['error_msg'] = 'Invalid Username or Password';
+					$this->session->set_flashdata('error_msg', $data['error_msg']);
+					redirect('/');
+				}
+			}
+	
 		}
+		public function dashboard() {
+			if(isset($this->session->userdata['logged_in'])){
+				
+				$this->load->view('admin/dashboard');
+				
+			}else{
+				$this->load->view('admin/login');
+			}
+		}
+		public function logout() {
+			// Removing session data
+			$sess_array = array(
+				'username' => ''
+			);
+			$this->session->unset_userdata('logged_in', $sess_array);
+			$data['display_msg'] = 'Successfully Logout';
+			$this->session->set_flashdata('display_msg', $data['display_msg']);
+			
+			redirect('/');
+
+
+			}
+			
 	}
---
+++ application/models/Admin.php
@@ -0,0 +1,41 @@
+<?php
+	class Admin extends CI_Model {
+		function __construct() {
+			parent::__construct();
+			$this->tbl_name = 'admin';
+		}
+
+		public function login($data = array()) {
+			$condition = "name =" . "'" . $data['username'] . "' AND " . "password =" . "'" . $data['password'] . "'";
+			$this->db->select('*');
+			$this->db->from($this->tbl_name);
+			$this->db->where($condition);
+			$this->db->limit(1);
+
+			$query = $this->db->get();
+
+			if($query->num_rows() == 1){
+				return TRUE;
+			}else{
+				return FALSE;
+			}
+		}
+
+		public function readAdminInfo($username){
+			$condition = "name =" . "'" . $username . "'";
+			$this->db->select('*');
+			$this->db->from($this->tbl_name);
+			$this->db->where($condition);
+			$this->db->limit(1);
+
+			$query = $this->db->get();
+
+			if ($query->num_rows() == 1) {
+				return $query->result();
+			} else {
+				return FALSE;
+			}
+		}
+	}
+
+	?>
