4caaf1bfed8d46a0d00182b958f7fde93b49cc8b
Edison Villarta <edtvillarta@outlook.com>
2018-02-22 12:04:41 +0800

    Adjust templates

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,1 +1,5 @@
+	<script src="<?php echo base_url('/assets/js/custom.js'); ?>"></script>
+	<script src="<?php echo base_url('/assets/js/bootstrap.min.js'); ?>"></script>
+
+</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,8 +8,5 @@
 	<title><?php echo $title_page; ?> | Market View Research</title>
 	<?php echo link_tag('assets/css/bootstrap.min.css'); ?>
 	<?php echo link_tag('assets/css/custom.css'); ?>
-	<!-- <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script> -->
-	<script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
-	
 </head>
 
