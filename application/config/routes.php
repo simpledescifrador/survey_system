e9ec2b84328330c0448a3d8ed1a930f9baf88e9e
Edison Villarta <edtvillarta@outlook.com>
2018-02-22 12:04:55 +0800

    add routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['logout'] = 'admin_controller/logout';
-$route['login'] = 'admin_controller/loginAuth'; 
+$route['login'] = 'admin_controller/loginAuth';
+$route['dashboard'] = 'admin_controller/dashboard';
 $route['default_controller'] = 'admin_controller/dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
